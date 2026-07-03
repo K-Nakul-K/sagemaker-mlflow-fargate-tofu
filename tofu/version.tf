a1344537d4a12c4067661be9e2a35efbd032c7db
Nakul Khargonkar <nakulkhargonkar@gmail.com>
2026-07-03 20:59:00 +0530

    Add Terraform configuration for AWS provider

---
+++ tofu/version.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.28.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.9.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = local.default_tags
+  }
+}
